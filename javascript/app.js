c26ddc49a4accbed4e0c474c474e536122031bba
Pat Marshall <pmarshall1716@gmail.com>
2016-06-13 19:00:30 -0600

    history showing in text box

--- javascript/app.js
+++ javascript/app.js
@@ -1,16 +1,26 @@
+var history = "";
+
 window.onload = function () {
 	var result = document.getElementById('result');
-};  
+}
 
 function addValue(i) {
-result.innerHTML += i	
+result.innerHTML += i;
 
 }
 
 function calculate(i) {
-	result.innerHTML = eval(result.innerHTML)
+	result.innerHTML = eval(result.innerHTML);
+	// history += eval(result.innerHTML);
+	var history = parseInt(eval(result.innerHTML));
+	var historyBox = document.getElementById('history');
+	historyBox.innerHTML += history;
 }
 
 function clearResults() {
  result.innerHTML = "";
 }
+
+
+
+
